355fcf7b5d513f3e5ba922482d370851ab9bd354
Agus Saputra <syahputraagus988@gmail.com>
2025-06-26 20:44:01 +0700

    update web.php

--- projecttimalam/routes/web.php
+++ projecttimalam/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 use App\Http\Controllers\Biodatacontroller;
 
+use App\Models\Prodi;
+use App\Models\Matakuliah;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,4 +12,38 @@ Route::get('/biodata', [Biodatacontroller::class, 'index']);
 Route::post('/biodata', [Biodatacontroller::class, 'proses']);
 Route::post('/mahasiswa', [Biodatacontroller::class, 'proses2']);
 Route::get('/mahasiswa', [BiodataController::class, 'mahasiswa']);
+Route::get('/matakuliah/create/{x}/{y}', function ($nama,$sks) {
+    Matakuliah::create([
+        'nama' => $nama,
+        'sks' => $sks
+    ]);
+    return "Berhasil menambah data!";
+});
+
+Route::get('matakuliah/lihat', function () {
+    $matakuliah = Matakuliah::all();
+    foreach ($matakuliah as $data) {
+        echo $data->id." : ";
+        echo $data->nama." ";
+        echo $data->sks;
+        echo "<hr>";
+    }
+});
+
+Route::get('prodi/create/{x}', function ($nama) {
+    Prodi::create([
+        'nama' => $nama
+    ]);
+    return "Berhasil menambah data!";
+});
+
+Route::get('prodi/lihat', function () {
+    $prodi = Prodi::all();
+    foreach ($prodi as $data) {
+        echo $data->id." : ";
+        echo $data->nama." ";
+        echo "<hr>";
+    }
+});
+
 //mvc = model view controller
